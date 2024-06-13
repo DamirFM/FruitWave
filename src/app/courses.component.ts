a799860b9ff969341e644fdac719462707812eac
DamirFM <yakupovdamir93@gmail.com>
2024-06-13 13:13:01 -0400

    added CoursesComponent

---
+++ src/app/courses.component.ts
@@ -0,0 +1,7 @@
+// We need to import the Component decorator from the Angular core library.
+// Component decorator is a function that takes a metadata object that tells Angular how to create a component.
+import { Component } from '@angular/core';
+
+export class CoursesComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// starting point of the application
+// This file is used to bootstrap the application
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
