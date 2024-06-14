0f93b29c5140a101adfe9a9d2dea0411f3c1fa2f
DamirFM <yakupovdamir93@gmail.com>
2024-06-14 19:34:06 -0400

    added cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
 {  
   path: 'home', 
   component: HomeComponent},
 // default route redirect to home
+{  
+  path: 'cart', 
+  component: CartComponent},
 {
 path: '', 
 redirectTo: 'home', 
